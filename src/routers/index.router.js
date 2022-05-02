dc64016a5175eb7bb0c9d15c8af46b844eec9759
diegosgil <74438878+diegosgil@users.noreply.github.com>
2022-05-01 21:48:30 -0500

    Update index.router.js

--- src/routers/index.router.js
+++ src/routers/index.router.js
@@ -7,10 +7,15 @@ const estudiantes_controller = require('../controllers/estudiantes.controller');
 
 const vrs = '/api/v1';
 
+//carreras
 router.get(vrs + '/carreras', carreras_controller.consultarCarreras);
 router.get(vrs + '/carreras/:id', carreras_controller.consultarCarrera);
 router.put(vrs + '/carreras/:id', carreras_controller.actualizarCarrera);
 router.delete(vrs + '/carreras/:id', carreras_controller.eliminarCarrera);
 router.post(vrs + '/carreras', carreras_controller.crearCarrera);
+
+//estudiantes
+router.post(vrs + '/estudiantes', estudiantes_controller.crearEstudiante);
+router.get(vrs + '/estudiantes', estudiantes_controller.consultarEstudiantes);
 
 module.exports = router;
