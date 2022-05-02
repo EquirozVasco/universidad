e73f22273cdf26ab03eae1cc2e62dd05f2f2d49f
VargasSebas21 <78024501+VargasSebas21@users.noreply.github.com>
2022-05-01 21:45:03 -0500

    Se crea "crear estudiante"

---
+++ src/controllers/estudiantes.controller.js
@@ -0,0 +1,37 @@
+const Estudiantes = require('../models/estudiantes.models');
+const Carrera = require('../models/carreras.models');
+
+
+const crearEstudiante = async (req, res) => {
+    let respuesta = {};
+    try {
+        let { nro_id, nombre_1, nombre_2, apellido_1, apellido_2, fecha_de_nacimiento, carrera } = req.body;
+        let resultado = await Carrera.findOne({ nombre: carrera });
+        if (resultado != null) {
+            if (resultado.activo == true) {
+                respuesta.ok = true;
+                respuesta.message = "Estudiante creado exitosamente";
+                const estudiante = new Estudiantes({ nro_id, nombre_1, nombre_2, apellido_1, apellido_2, fecha_de_nacimiento, carrera });
+                resultado = await estudiante.save();
+                console.log(resultado);
+                respuesta.info = resultado;
+                res.send(respuesta);
+            } else {
+                console.log('Carrera inactiva.');
+                respuesta.info = 'Carrera inactiva';
+                res.send(respuesta);
+            }
+        } else {
+            console.log('Carrera inexistente');
+            respuesta.info = 'Carrera inexistente';
+            res.send(respuesta);
+        }
+    } catch (error) {
+        respuesta.ok = false;
+        respuesta.message = "Ha ocurrido un error creando el estudiante";
+        respuesta.info = error;
+        res.status(500).send(respuesta);
+    }
+}
+
+module.exports = { crearEstudiante }
